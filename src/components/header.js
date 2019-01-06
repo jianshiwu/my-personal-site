19e4b3d7ef273ce395bb81d79ab65259c14d8aa2
Jianshi Wu <jianshiwu@Wus-MacBook-Pro.local>
2019-01-06 16:29:17 -0500

    Move Header to the States ad Event format

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,50 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState ({ hasScrolled: true })
+    } else {
+      this.setState({hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="SiteName">
+          Jianshi Wu
+        </div>
+        <div className="Tabs">
+          <Link to="About">About</Link>
+          <Link to="Work">Work</Link>
+          <Link to="Blog">Blog</Link>
+        </div>
+        <div className="About">
+          <p>My name is Jianshi Wu, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
+          <p>Quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
+          <p>Explicabo. quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
+        </div>
+      </div>
+    )
+  }
+}
+
 
 export default Header
