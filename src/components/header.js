994f41dc9a387a9f48fa11f390fb2971857e2a3d
Jianshi Wu <jianshiwu@Wus-MacBook-Pro.fios-router.home>
2019-01-29 08:15:19 -0500

    misc

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 
+const HeaderGroup = styled.p`
+`
+
+const HeaderTitleText = styled.p`
+    font-size: 26px;
+    margin: 16px;
+    position: fixed;
+    cursor: pointer;
+`
+
+
+const Header = props => (
+  <HeaderGroup>
+    <HeaderTitleText>
+      Jianshi Wu
+    </HeaderTitleText>
+  </HeaderGroup>
+)
+
+export default Header
+
+/*
+
 class Header extends React.Component {
   constructor(props) {
     super(props)
@@ -48,3 +71,5 @@ class Header extends React.Component {
 
 
 export default Header
+
+*/
