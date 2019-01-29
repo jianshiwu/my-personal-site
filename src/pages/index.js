dd0ea0f2f28532f6dd93db3adc0866dfff6862e9
Jianshi Wu <jianshiwu@Wus-MacBook-Pro.fios-router.home>
2019-01-29 08:02:44 -0500

    misc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from '../components/Card'
 import Footer from '../components/Footer'
+import Header from '../components/Header';
+
+
 
 const IndexPage = () => (
   <div>
-   <div className="HeaderContainer">
-   </div>
+   <Header/>
    <div className="Gallery">
       <Card/>
       <Card/>
