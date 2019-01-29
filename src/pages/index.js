0f239bb5845a2dac4a13437d125d59bb9744dc2f
Jianshi Wu <jianshiwu@Wus-MacBook-Pro.fios-router.home>
2019-01-29 07:59:29 -0500

    delete header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Card from '../components/Card'
 import Footer from '../components/Footer'
-import Header from '../components/Header'
 
 const IndexPage = () => (
   <div>
    <div className="HeaderContainer">
-      <Header />
    </div>
    <div className="Gallery">
       <Card/>
