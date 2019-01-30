7fb9567b5b8cb9436951d1f331749d8698ef39a3
Jianshi Wu <jianshiwu@Wus-MacBook-Pro.fios-router.home>
2019-01-29 22:44:57 -0500

    misc

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,29 @@ const HeaderTitleText = styled.p`
     cursor: pointer;
 `
 
+const HeaderTabGroup = styled.div`
+    font-size: 26px;
+    position: fixed;
+    margin: 0px 0px 0px 36%;
+    transition: 0.7s cubic-bezier(0.075, 0.82, 0.165, 1);
+`
+
+const AboutTab = styled.p`
+`
+const WorksTab = styled.p`
+`
+const BlogTab = styled.p`
+`
+
+const HeaderAboutText = styled.div`
+    position: fixed;
+    right: 16px;
+    margin: 0px 16px 0px 66%;
+    max-width: 400px;
+    transition: 0.7s cubic-bezier(0.075, 0.82, 0.165, 1);
+`
+
+
 
 
 // End of component style
@@ -28,6 +51,16 @@ const Header = props => (
      <HeaderTitleText>
         Jianshi Wu
      </HeaderTitleText>
+     <HeaderTabGroup>
+         <AboutTab>About</AboutTab>
+         <WorksTab>Works</WorksTab>
+         <BlogTab>Blog</BlogTab>
+     </HeaderTabGroup>
+     <HeaderAboutText>
+      <p>My name is Jianshi Wu, explicabo. quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
+      <p>Quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
+      <p>Explicabo. quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
+     </HeaderAboutText>
     </HeaderGroup>
 )
 
