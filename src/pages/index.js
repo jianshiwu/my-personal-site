8c8c7435077492021f63445be419b611a70ad9bc
Jianshi Wu <jianshiwu@Wus-MacBook-Pro.local>
2019-08-10 11:18:07 -0400

    nothing

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from '../components/Card'
+import Footer from '../components/Footer'
+import Header from '../components/Header';
+
+
+const IndexPage = () => (
+  <div>
+   <Header/>
+   <div className="GalleryContainer">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+   </div>
+   <Footer/>
+  </div>
+)
+
+export default IndexPage
